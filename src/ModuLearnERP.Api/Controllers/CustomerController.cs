a3e22f894522530f896d9ea0e663a10af6bfc511
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2025-04-17 21:05:25 -0300

    Adicionar arquivos de projeto.

---
+++ src/ModuLearnERP.Api/Controllers/CustomerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuLearnERP.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public IActionResult Register([FromBody] string customer)
+        {
+            return Ok("Customer registered successfully");
+        }
+    }
+}
---
+++ src/ModuLearnERP.Api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuLearnERP.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] string user)
+        {
+            return Ok("User registered successfully");
+        }
+    }
+}
